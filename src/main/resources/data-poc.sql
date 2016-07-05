20857be32c3e35745210dc6804874fb2a02b2226
Sebastian Groh <sgroh@USSFO-SGROH.local>
2016-07-05 13:45:26 -0300

    DB changes to connect to the corresponding Mysql Instance

--- src/main/resources/data-poc.sql
+++ src/main/resources/data-poc.sql
@@ -1,13 +1,13 @@
 -- Users
-INSERT INTO user (id, username, email)  VALUES (1, 'sgroh', 'sebastian.groh@reflektion.com') ;
-INSERT INTO user (id, username, email)  VALUES (2, 'apersson', 'agustin.persson@reflektion.com');
-INSERT INTO user (id, username, email)  VALUES (3, 'jdoe', 'jon.doe@reflektion.com');
+INSERT INTO user (id, username, email, enabled)  VALUES (1, 'sgroh', 'sebastian.groh@reflektion.com', true) ;
+INSERT INTO user (id, username, email, enabled)  VALUES (2, 'apersson', 'agustin.persson@reflektion.com', true);
+INSERT INTO user (id, username, email, enabled)  VALUES (3, 'jdoe', 'jon.doe@reflektion.com', true);
 --
 -- Roles --
 --
-INSERT INTO role (id, name)  VALUES (1, 'basic');
-INSERT INTO role (id, name)  VALUES (2, 'expert');
-INSERT INTO role (id, name)  VALUES (3, 'admin');
+INSERT INTO role (id, name)  VALUES (1, 'Basic');
+INSERT INTO role (id, name)  VALUES (2, 'Expert');
+INSERT INTO role (id, name)  VALUES (3, 'Admin');
 --
 -- User-Roles --
 --
@@ -18,13 +18,13 @@ INSERT INTO user_roles (user_id, role_id)  VALUES (3, 1);
 --
 -- Permission --
 --
-INSERT INTO permission (id, permission)  VALUES (1, 000);
-INSERT INTO permission (id, permission)  VALUES (2, 010);
-INSERT INTO permission (id, permission)  VALUES (3, 011);
-INSERT INTO permission (id, permission)  VALUES (4, 100);
-INSERT INTO permission (id, permission)  VALUES (5, 101);
-INSERT INTO permission (id, permission)  VALUES (6, 110);
-INSERT INTO permission (id, permission)  VALUES (7, 111);
+INSERT INTO permission (id, permission)  VALUES (1, '000');
+INSERT INTO permission (id, permission)  VALUES (2, '010');
+INSERT INTO permission (id, permission)  VALUES (3, '011');
+INSERT INTO permission (id, permission)  VALUES (4, '100');
+INSERT INTO permission (id, permission)  VALUES (5, '101');
+INSERT INTO permission (id, permission)  VALUES (6, '110');
+INSERT INTO permission (id, permission)  VALUES (7, '111');
 --
 -- Key --
 --
