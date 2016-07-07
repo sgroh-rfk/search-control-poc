d7f7f5d6a9b75bc24f68621c358f48cf623b0150
Sebastian Groh <sebastian.groh@reflektion.com>
2016-07-07 15:12:55 -0300

    [DASH-610] insert only in not exists the tuple

--- src/main/resources/data-poc.sql
+++ src/main/resources/data-poc.sql
@@ -1,40 +1,40 @@
 -- Users
-INSERT INTO user (id, username, email, enabled)  VALUES (1, 'sgroh', 'sebastian.groh@reflektion.com', true) ;
-INSERT INTO user (id, username, email, enabled)  VALUES (2, 'apersson', 'agustin.persson@reflektion.com', true);
-INSERT INTO user (id, username, email, enabled)  VALUES (3, 'jdoe', 'jon.doe@reflektion.com', true);
+INSERT IGNORE INTO user (id, username, email, enabled)  VALUES (1, 'sgroh', 'sebastian.groh@reflektion.com', true) ;
+INSERT IGNORE INTO user (id, username, email, enabled)  VALUES (2, 'apersson', 'agustin.persson@reflektion.com', true);
+INSERT IGNORE INTO user (id, username, email, enabled)  VALUES (3, 'jdoe', 'jon.doe@reflektion.com', true);
 --
 -- Roles --
 --
-INSERT INTO role (id, name)  VALUES (1, 'Basic');
-INSERT INTO role (id, name)  VALUES (2, 'Expert');
-INSERT INTO role (id, name)  VALUES (3, 'Admin');
+INSERT IGNORE INTO role (id, name)  VALUES (1, 'Basic');
+INSERT IGNORE INTO role (id, name)  VALUES (2, 'Expert');
+INSERT IGNORE INTO role (id, name)  VALUES (3, 'Admin');
 --
 -- User-Roles --
 --
-INSERT INTO user_roles (user_id, role_id)  VALUES (1, 3);
-INSERT INTO user_roles (user_id, role_id)  VALUES (1, 2);
-INSERT INTO user_roles (user_id, role_id)  VALUES (2, 2);
-INSERT INTO user_roles (user_id, role_id)  VALUES (3, 1);
+INSERT IGNORE INTO user_roles (user_id, role_id)  VALUES (1, 3);
+INSERT IGNORE INTO user_roles (user_id, role_id)  VALUES (1, 2);
+INSERT IGNORE INTO user_roles (user_id, role_id)  VALUES (2, 2);
+INSERT IGNORE INTO user_roles (user_id, role_id)  VALUES (3, 1);
 --
 -- Permission --
 --
-INSERT INTO permission (id, permission)  VALUES (1, '000');
-INSERT INTO permission (id, permission)  VALUES (2, '010');
-INSERT INTO permission (id, permission)  VALUES (3, '011');
-INSERT INTO permission (id, permission)  VALUES (4, '100');
-INSERT INTO permission (id, permission)  VALUES (5, '101');
-INSERT INTO permission (id, permission)  VALUES (6, '110');
-INSERT INTO permission (id, permission)  VALUES (7, '111');
+INSERT IGNORE INTO permission (id, permission)  VALUES (1, '000');
+INSERT IGNORE INTO permission (id, permission)  VALUES (2, '010');
+INSERT IGNORE INTO permission (id, permission)  VALUES (3, '011');
+INSERT IGNORE INTO permission (id, permission)  VALUES (4, '100');
+INSERT IGNORE INTO permission (id, permission)  VALUES (5, '101');
+INSERT IGNORE INTO permission (id, permission)  VALUES (6, '110');
+INSERT IGNORE INTO permission (id, permission)  VALUES (7, '111');
 --
 -- Key --
 --
-INSERT INTO keytable (id, description, name, `schema`, parent_id )  VALUES (1, '--', 'noname', null, null);
+INSERT IGNORE INTO keytable (id, description, name, `schema`, parent_id )  VALUES (1, '--', 'noname', null, null);
 --
 -- Permission - roles --
 --
-INSERT INTO permission_role (role_id, permission_id)  VALUES (3, 7);
-INSERT INTO permission_role (role_id, permission_id)  VALUES (2, 6);
-INSERT INTO permission_role (role_id, permission_id)  VALUES (1, 2);
+INSERT IGNORE INTO permission_role (role_id, permission_id)  VALUES (3, 7);
+INSERT IGNORE INTO permission_role (role_id, permission_id)  VALUES (2, 6);
+INSERT IGNORE INTO permission_role (role_id, permission_id)  VALUES (1, 2);
 --
 -- Permission - keys --
 --
